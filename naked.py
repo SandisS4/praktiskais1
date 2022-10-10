a4e2bbcb64b648a2228b8719f5a606d2e1ef2adf
Sandis <sandiss2013@gmail.com>
2022-10-10 13:03:28 +0000

    ierakstiju nasa api atslegu

--- naked.py
+++ naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "???"
+nasa_api_key = "OOIEOudOB4dacaPgthiodkRIjfLddzDOgp0hYavQ"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
